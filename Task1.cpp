c02ba64cba2a162d1f8810a1c6a3c4678a8a9e0c
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-01-25 19:04:55 +0300

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -1,4 +1,4 @@
-/*
+/* Контрольная работа 25.01.2018 Вариант 2 Задание 1
 Массив из 5 элементов
 найти мин, макс,
 если мин>0 найти произведение
@@ -12,7 +12,7 @@
 
 using namespace std;
 // ФУНКЦИЯ ДЛЯ ВЫВОДА МАССИВА :
-void printOurArr(int a[SIZE]);
+void printOurArr(int a[], int size);
 
 void main() {
 	setlocale(LC_ALL, "rus");
@@ -24,12 +24,12 @@ void main() {
 		cin >> ourArr[i];
 	}
 	cout << "Исходный массив:\n";
-	printOurArr(ourArr);
+	printOurArr(ourArr, SIZE);
 	int max = ourArr[0];
 	int iMax = 0;
 	int min = ourArr[0];
 	int iMin = 0;
-	for (int i = 0; i < SIZE - 1; i++)
+	for (int i = 1; i < SIZE; i++)
 	{
 		if (ourArr[i] < min) {
 			min = ourArr[i];
@@ -41,16 +41,47 @@ void main() {
 			iMax = i;
 		}
 	}
-	cout << "Минимальное значение: " << min << endl;
+	cout << "\nМинимальное значение: " << min << endl;
 	cout << "Максимальное значение: " << max << endl;
-
-
-	cout << "Итоговый массив:\n";
-	printOurArr(ourArr); // Вызов функции вывода массива
+	int tmp=ourArr[iMax];
+	ourArr[iMax] = ourArr[iMin];
+	ourArr[iMin] = tmp;
+	cout << "Массив с перестановкой максимальногои минимального значений:\n";
+	printOurArr(ourArr, SIZE);
+	if (min > 0) {
+		int sum = 0;
+		for (int i = 0; i < SIZE; i++) sum += ourArr[i];
+		cout << "Сумма элементов равна: " << sum << endl;
+	}
+	else {
+		int product = 1;
+		for (int i = 0; i < SIZE; i++) product *= ourArr[i];
+		cout << "Произведение элементов равно: " << product << endl;
+	}
+	int k = SIZE - 1;
+	int flag = true;
+	while (k > 0 && flag)
+	{
+		int endI = -1;
+		flag = false;
+		for (int i = 0; i < k; i++)
+			if (ourArr[i] > ourArr[i + 1]) {
+				int tmp = ourArr[i];
+				ourArr[i] = ourArr[i + 1];
+				ourArr[i + 1] = tmp;
+				flag = true;
+				endI = i;
+			}
+		k = endI;
+	}
+	cout << "Итоговый массив:\n" ;
+	printOurArr(ourArr, SIZE); // Вызов функции вывода массива
 	system("pause");
 }
-void printOurArr(int a[], int size) {
+void printOurArr(int a[], int size) 
+{
 	for (int i = 0; i < size; i++) {
 		cout << a[i] << " ";
 	}
 }
+
